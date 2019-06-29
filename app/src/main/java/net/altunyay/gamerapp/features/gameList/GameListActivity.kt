4bb0f8e012456e3fbdcb7e9272e97d26f9e4c398
ardaaltunyay <arda.altunyay@appcent.mobi>
2019-06-29 23:10:25 +0300

    GameListActivity and SettingsActivity(AppWindowAnimation added in styles.xml) edited

--- app/src/main/java/net/altunyay/gamerapp/features/gameList/GameListActivity.kt
+++ app/src/main/java/net/altunyay/gamerapp/features/gameList/GameListActivity.kt
@@ -1,18 +1,18 @@
-package net.altunyay.gamerapp.features
+package net.altunyay.gamerapp.features.gameList
 
 import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.coroutineScope
+import kotlinx.android.synthetic.main.activity_game_list.*
 import net.altunyay.gamerapp.R
 import net.altunyay.gamerapp.common.base.BaseActivity
 import net.altunyay.gamerapp.features.settings.SettingsActivity
 
-class MainActivity : BaseActivity() {
+class GameListActivity : BaseActivity() {
 
-    override fun getContentViewId(): Int = R.layout.activity_main
+    override fun getContentViewId(): Int = R.layout.activity_game_list
 
     override fun onCreateFinished(savedInstanceState: Bundle?) {
+        title = "Welcome"
         btnSettings.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
--- app/src/main/java/net/altunyay/gamerapp/features/settings/SettingsActivity.kt
+++ app/src/main/java/net/altunyay/gamerapp/features/settings/SettingsActivity.kt
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_settings.*
 import net.altunyay.gamerapp.R
 import net.altunyay.gamerapp.common.base.BaseActivity
-import net.altunyay.gamerapp.common.utils.ClientPreferences
+import net.altunyay.gamerapp.common.preferences.ClientPreferences
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SettingsActivity : BaseActivity() {
 
     private val preferences : ClientPreferences by inject()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+    override fun getContentViewId(): Int = R.layout.activity_settings
 
+    override fun onCreateFinished(savedInstanceState: Bundle?) {
+        window.setWindowAnimations(R.style.AppWindowAnimation)
+        title = "Settings"
         if(preferences.isDarkModeEnabled()) smDarkMode.isChecked = true
 
         smDarkMode.setOnCheckedChangeListener { _, isChecked ->
