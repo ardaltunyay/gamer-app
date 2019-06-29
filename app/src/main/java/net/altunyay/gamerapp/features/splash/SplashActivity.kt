9f7c037955842daad2379e1aa5a604ed7185ae4d
ardaaltunyay <arda.altunyay@appcent.mobi>
2019-06-29 23:09:22 +0300

    SplashActivity created

---
+++ app/src/main/java/net/altunyay/gamerapp/features/splash/SplashActivity.kt
@@ -0,0 +1,21 @@
+package net.altunyay.gamerapp.features.splash
+
+import android.content.Intent
+import android.os.Bundle
+import kotlinx.coroutines.*
+import net.altunyay.gamerapp.R
+import net.altunyay.gamerapp.common.base.BaseActivity
+import net.altunyay.gamerapp.features.gameList.GameListActivity
+
+class SplashActivity : BaseActivity() {
+
+    override fun getContentViewId(): Int = R.layout.activity_splash
+
+    override fun onCreateFinished(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
+        GlobalScope.launch(Dispatchers.Main){
+            delay(1500)
+            startActivity(Intent(this@SplashActivity, GameListActivity::class.java))
+        }
+    }
+}
