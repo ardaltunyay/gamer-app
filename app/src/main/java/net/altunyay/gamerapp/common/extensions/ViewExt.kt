386143ffb5b4b06d23bdaf1fd798aeba938ec575
Arda Altunyay <ardaltunyay@gmail.com>
2019-07-02 01:36:14 +0300

    View extensions class created

---
+++ app/src/main/java/net/altunyay/gamerapp/common/extensions/ViewExt.kt
@@ -0,0 +1,15 @@
+package net.altunyay.gamerapp.common.extensions
+
+import android.view.View
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
